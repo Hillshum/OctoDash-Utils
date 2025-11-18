68f5f2faee6e4bcf3c079f1ca0c2691353dddcb2
Hilton Shumway <hillshum@gmail.com>
2025-11-18 17:28:23 +0000

    Better manage artifacts
    Delete old ones before download and put them in separate dir

--- get-latest-build.sh
+++ get-latest-build.sh
@@ -4,6 +4,8 @@ DEFAULT_BRANCH=main
 
 BRANCH=${1:-$DEFAULT_BRANCH}
 
+rm -r artifacts/*
+
 RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
 
-gh -R UnchartedBull/OctoDash run download $RUN_ID
+gh -R UnchartedBull/OctoDash run download --dir artifacts $RUN_ID
