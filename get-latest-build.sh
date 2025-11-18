b850d6d6a176376e3501ea80a5f1d1b2e30b0f5c
Hilton Shumway <hillshum@gmail.com>
2025-11-18 08:12:46 +0000

    Allow for specifiying branch

--- get-latest-build.sh
+++ get-latest-build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b plugin-migration -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
+DEFAULT_BRANCH=main
+
+BRANCH=${1:-$DEFAULT_BRANCH}
+
+RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
 
 gh -R UnchartedBull/OctoDash run download $RUN_ID
