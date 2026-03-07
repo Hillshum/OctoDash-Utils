6c692302821153e75f8994edcf70afa10c9b5e3b
Hilton Shumway <hillshum@gmail.com>
2026-03-07 12:04:13 +0000

    Use run id from variable
    derp....

--- get-latest-build.sh
+++ get-latest-build.sh
@@ -10,7 +10,7 @@ mkdir -p artifacts
 RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
 
 
-INFO=$(gh api /repos/UnchartedBull/OctoDash/actions/runs/22778707719/artifacts | jq '.artifacts[]  | select(.name | contains(".whl"))')
+INFO=$(gh api /repos/UnchartedBull/OctoDash/actions/runs/$RUN_ID/artifacts | jq '.artifacts[]  | select(.name | contains(".whl"))')
 NAME=$(echo $INFO | jq -r '.name')
 URL=$(echo $INFO | jq -r '.archive_download_url')
 echo "Downloading $NAME from $URL"
