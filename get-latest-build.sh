f61ac438e637312e9f49a02b077287ad1c21f74a
Hilton Shumway <hillshum@gmail.com>
2026-03-07 10:21:26 +0000

    Updates for handling non-zipped archive

--- get-latest-build.sh
+++ get-latest-build.sh
@@ -8,4 +8,10 @@ rm -r artifacts/*
 
 RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
 
-gh -R UnchartedBull/OctoDash run download --dir artifacts $RUN_ID
+
+INFO=$(gh api /repos/UnchartedBull/OctoDash/actions/runs/22778707719/artifacts | jq '.artifacts[]  | select(.name | contains(".whl"))')
+NAME=$(echo $INFO | jq -r '.name')
+URL=$(echo $INFO | jq -r '.archive_download_url')
+echo "Downloading $NAME from $URL"
+
+gh api $URL > artifacts/$NAME
