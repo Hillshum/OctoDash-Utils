e3f64fdf2f3228fd14c2cf19a110e3b9fdef6296
Hilton Shumway <hillshum@gmail.com>
2026-03-21 13:26:56 +0000

    Error when build not finished

--- get-latest-build.sh
+++ get-latest-build.sh
@@ -7,7 +7,17 @@ BRANCH=${1:-$DEFAULT_BRANCH}
 rm -r artifacts/*
 mkdir -p artifacts
 
-RUN_ID=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId -q '.[0].databaseId')
+RUN=$(gh -R UnchartedBull/OctoDash run list -b $BRANCH -w CI -L 1 --json name,number,databaseId,status | jq -r '.[0]')
+if [  "$RUN" == "null" ]; then
+  echo "No runs found for branch $BRANCH"
+  exit 1
+fi
+if [ "$(echo $RUN | jq -r '.status')" != "completed" ]; then
+  echo "Latest run for branch $BRANCH is not completed"
+  exit 1
+fi
+
+RUN_ID=$(echo $RUN | jq -r '.databaseId')
 
 
 INFO=$(gh api /repos/UnchartedBull/OctoDash/actions/runs/$RUN_ID/artifacts | jq '.artifacts[]  | select(.name | contains(".whl"))')
